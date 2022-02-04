2748d2cd5aa1717f2b2b20b2083acf27460c99f1
kamau-cyrpto <kamamupeter343@gmail.com>
2022-02-04 10:39:54 +0300

    Added description

--- v/code/vehicle_flow_report.php
+++ v/code/vehicle_flow_report.php
@@ -1,68 +1,66 @@
-<?php
-//
-?>
+<!-- This is a report of the vehicle flow in the mutall carpark -->
 <html>
 
-    <head>
-        <meta charset="UTF-6">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+P+One&family=Poppins:wght@300&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="report.css">
-    </head>
+<head>
+    <meta charset="UTF-6">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+P+One&family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="report.css">
+</head>
 
-    <body>
-        <div class="results">
-            <form class="vehicle">
-                <h1>VEHICLE FLOW REPORT</h1>
-                <?php
-                //CREATE THE HTML TABLE
+<body>
+    <div class="results">
+        <form class="vehicle">
+            <h1>VEHICLE FLOW REPORT</h1>
+            <?php
+            //CREATE THE HTML TABLE
+            //
+            //Fetch the contents of the JSON file
+            $array_data = file_get_contents("vehicle_flow_result.json");
+            //
+            //Decode the data into associative arrays
+            $string_data = json_decode($array_data);
+            //
+            //Obtain the array's values in the reverse order
+            $rows = array_reverse(array_values($string_data));
+            //
+            //Open a html table
+            $table = "<table>";
+            //
+            //Define the column headings of the table
+            $table .= "<tr><th> Registration Number</th>";
+            $table .= "<th> Category</th>";
+            $table .= "<th>Flow</th>";
+            $table .= "<th>Datetime</th>";
+            $table .= "<th>Operator</th></tr>";
+            //
+            $tr = '';
+            //
+            //Generate the rows and columns
+            foreach ($rows as $row) {
                 //
-                //Fetch the contents of the JSON file
-                $array_data = file_get_contents("vehicle_flow_result.json");
+                $tr .= "<tr>";
                 //
-                //Decode the data into associative arrays
-                $string_data = json_decode($array_data);
-                //
-                //Obtain the array's values in the reverse order
-                $rows = array_reverse(array_values($string_data));
-                //
-                //Open a html table
-                $table = "<table>";
-                //
-                //Define the column headings of the table
-                $table .= "<tr><th> Registration Number</th>";
-                $table .= "<th> Category</th>";
-                $table .= "<th>Flow</th>";
-                $table .= "<th>Datetime</th>";
-                $table .= "<th>Operator</th></tr>";
-                //
-                $tr = '';
-                //
-                //Generate the rows and columns
-                foreach ($rows as $row) {
+                foreach ($row as $value) {
                     //
-                    $tr .= "<tr>"; 
-                    //
-                    foreach ($row as $value) {
-                        //
-                        $tr .= "<td>$value</td>";
-                    }
-                    //
-                    $tr .= "</tr>";
+                    $tr .= "<td>$value</td>";
                 }
                 //
-                $table .= $tr;
-                //
-                //Close the table
-                $table .= "</table>";
-                //
-                //return the contents of the table
-                echo $table;
-                ?>
-            </form>
-        </div>
-    </body>
+                $tr .= "</tr>";
+            }
+            //
+            $table .= $tr;
+            //
+            //Close the table
+            $table .= "</table>";
+            //
+            //return the contents of the table
+            echo $table;
+            ?>
+        </form>
+    </div>
+</body>
 
 </html>
