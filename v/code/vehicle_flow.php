a1640f7dd27ef74dfdb2ff6845f30b23c458a031
kamau-cyrpto <kamamupeter343@gmail.com>
2022-02-04 10:56:12 +0300

    "styling correction"

--- v/code/vehicle_flow.php
+++ v/code/vehicle_flow.php
@@ -13,23 +13,27 @@ date_default_timezone_set('Africa/Nairobi');
 
 ?>
 <!<!doctype html>
-<html>
+    <html>
+
     <head>
         <title>vehicle_flow</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     
+        <!-- There is an issue with styling of this document. 
+                It is unusual for the style of the code to be more than the actual content
+            -->
         <style>
-
             p {
                 font-size: 30px;
             }
-             h3 {
-               margin-left: 25%;
-               color: teal;
+
+            h3 {
+                margin-left: 25%;
+                color: teal;
             }
-            .frame{
+
+            .frame {
                 margin: auto;
-                margin-top:0%;
+                margin-top: 0%;
                 border: 1px solid black;
                 border-radius: 4px;
                 box-sizing: border-box;
@@ -37,29 +41,33 @@ date_default_timezone_set('Africa/Nairobi');
                 width: 40%;
                 padding-left: 5%;
             }
-            input[type='text'],[type='date']{
-                width:35%;
-                height:5%;
-                background-color:whitesmoke;
-                color:black;
-                font-size:20px;
-                margin-top:2%;
-               
+
+            input[type='text'],
+            [type='date'] {
+                width: 35%;
+                height: 5%;
+                background-color: whitesmoke;
+                color: black;
+                font-size: 20px;
+                margin-top: 2%;
+
             }
-            input[type="radio"]{
+
+            input[type="radio"] {
                 margin-right: 2%;
                 margin-left: 4%;
             }
-            label{
+
+            label {
                 display: block;
-                 margin-top: 2%;
+                margin-top: 2%;
             }
 
-            input[type='submit']{
-                width:20%;
-                height:40px;
-                font-size:30px;
-                margin-top:10%;
+            input[type='submit'] {
+                width: 20%;
+                height: 40px;
+                font-size: 30px;
+                margin-top: 10%;
                 background-color: silver;
                 margin-left: 60%;
                 margin-bottom: 2%;
@@ -72,51 +80,43 @@ date_default_timezone_set('Africa/Nairobi');
 
             /*
             Style the page for mobile devices. */
-            @media screen and (max-width:480px){
-                .frame{
+            @media screen and (max-width:480px) {
+                .frame {
                     width: 95%;
                 }
-                input[type='submit']{
-                    width:35%;
+
+                input[type='submit'] {
+                    width: 35%;
 
                 }
             }
-
         </style>
 
 
     </head>
+
     <body>
-       <form class="frame"action="vehicle_flow_results.php" method="post">
-           <h3>Vehicle Flow</h3>
-             <p> Registration Number</p>
-            <input 
-                type="text" 
-                style="text-transform:uppercase" 
-                name="numberplate" 
-                placeholder="Number Plate"
-                maxlength="7" 
-                required="true"/>
+        <form class="frame" action="vehicle_flow_results.php" method="post">
+            <h3>Vehicle Flow</h3>
+            <p> Registration Number</p>
+            <input type="text" style="text-transform:uppercase" name="numberplate" placeholder="Number Plate" maxlength="7" required="true" />
             <!-- 
             Vehicle category. -->
             <p>Vehicle Category:</p>
-            <label><input type="radio" name="category" value="car"/>Car</label>
-            <label><input type="radio"name="category" value="lorry"/>Lorry</label>
-            <label><input type="radio"name="category" value="motorcycle"/>Motorcycle</label>
+            <label><input type="radio" name="category" value="car" />Car</label>
+            <label><input type="radio" name="category" value="lorry" />Lorry</label>
+            <label><input type="radio" name="category" value="motorcycle" />Motorcycle</label>
             <!--
             Vehicle flow. -->
             <p>Direction of the vehicle:</p>
-            <label><input type="radio" name="flow" value="incoming"/>Incoming </label>
-            <label><input type="radio"name="flow" value="outgoing"/>Outgoing</label>
+            <label><input type="radio" name="flow" value="incoming" />Incoming </label>
+            <label><input type="radio" name="flow" value="outgoing" />Outgoing</label>
 
-               <input type="hidden" value="<?php echo $datetime; ?>" name="datetime" />
-             <input 
-                type='text' 
-                name="email" 
-                value="<?php echo $_SESSION['email']; ?>" 
-                hidden='true'/>
-            <input type="submit"name="submit" value="submit"/>
+            <input type="hidden" value="<?php echo $datetime; ?>" name="datetime" />
+            <input type='text' name="email" value="<?php echo $_SESSION['email']; ?>" hidden='true' />
+            <input type="submit" name="submit" value="submit" />
         </form>
 
     </body>
-</html>
+
+    </html>
