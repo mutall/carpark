49cc35ed97da62059ef09e1a3f59948cadcfcb4b
PMuraya <peterkmuraya@gmail.com>
2022-02-14 13:16:06 +0300

    Peter muraya's version
    Basic styling demonstration

--- v/code/stock.php
+++ v/code/stock.php
@@ -16,71 +16,30 @@ $date = date("Y-m-d");
 <html>
     <head>
         <title>stock</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <style>
-            p {
-                font-size: 30px;
-                margin-top: 3%;
-            }
-             h3 {
-               margin-left: 25%;
-               color: teal;
-               margin-top: 0px;
-            }
-            .frame{
-                margin: auto;
-                border: 1px solid black;
-                border-radius: 4px;
-                box-sizing: border-box;
-                font-size: 25px;
-                width: 40%;
-                padding-left: 5%;
-            }
-            input[type='text'],[type='date']{
-                width:50%;
-                background-color:whitesmoke;
-                font-size:20px;
-                margin-top:3px;
-                padding-top:5px;
-            }
-
-            input[type='radio']{
-                margin-right: 2%;
-                margin-left: 4%;
-
+            *{
+                font-size:30px;
             }
             label{
-                display: block;
-                margin-top: 2%;
+                display:block
+            }
+            
+            form{
+                width:50%;
+                margin-left:25%;
+            }
+            
+            fieldset{
+                width:60%;
+                margin-top:5%;
+                line-height:1.5;
             }
             input[type='submit']{
-                width:20%;
-                font-size:30px;
-                margin-top:10%;
-                background-color: silver;
-                margin-left: 60%;
-                margin-bottom: 2%;
-                cursor: pointer;
+                position:absolute;
+                top:50%;
+                left:60%;
             }
-
-            input[type='submit']:hover {
-                background-color: #3e8e41
-            }
-
-            /*
-            Style the page for mobile devices. */
-            @media screen and (max-width:480px){
-                .frame{
-                    width: 100%;
-                }
-                input[type='submit']{
-                    width:35%;
-
-                }
-                input[type='text'],[type='date']{
-                    width:40%;
-                } 
-            }
+            
         </style> 
     </head>
 
@@ -88,24 +47,28 @@ $date = date("Y-m-d");
 
         <form class="frame" action="stock_results.php" method="post">  <!--Car registration details. -->
             <h3>Vehicle Stock</h3>
-            <p>Car Registration Number:</p>
-            <input 
-                type="text" 
-                style="text-transform:uppercase" name="numberplate" maxlength="7"
-                pattern="\w{3}\d{3}\w" title="Use this format instead KBZ999Z" 
-                required />
+            <label>Car Registration Number:
+                <input 
+                    type="text" 
+                    style="text-transform:uppercase" name="numberplate" maxlength="7"
+                    pattern="\w{3}\d{3}\w" title="Use this format instead KBZ999Z" 
+                    required />
+            </label>
             <!-- 
             pattern="\w{3}\d{3}\w" title="Use this format instead KBZ999Z"-->
-            <p>Vehicle Category:</p>
-            <label><input type="radio" name="category" value="Car" required />Car</label>
-            <label><input type="radio" name="category" value="Lorry" required />Lorry</label>
-            <label><input type="radio" name="category" value="Motorcycle" required />Motorcycle</label>
-
-            <p>Time of the Day:</p>
-            <label><input type="radio" name="daytime" value="morning" required />Morning</label>
-            <label><input type="radio" name="daytime" value="mid_day" required />Mid-day</label>
-            <label><input type="radio" name="daytime" value="evening" required />Evening</label>
-
+            <fieldset>
+                <legend>Vehicle Category:</legend>
+                <label><input type="radio" name="category" value="Car" required />Car</label>
+                <label><input type="radio" name="category" value="Lorry" required />Lorry</label>
+                <label><input type="radio" name="category" value="Motorcycle" required />Motorcycle</label>
+            </fieldset>
+            
+            <fieldset>   
+                <legend>Time of the Day:</legend>
+                <label><input type="radio" name="daytime" value="morning" required />Morning</label>
+                <label><input type="radio" name="daytime" value="mid_day" required />Mid-day</label>
+                <label><input type="radio" name="daytime" value="evening" required />Evening</label>
+            </fieldset>
             <input type="hidden" value="<?php echo $datetime; ?>" name="datetime" />
             
             <input 
